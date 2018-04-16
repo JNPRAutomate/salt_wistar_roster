a57c4ca4772bdd42b5ef856c478b3d2e2f49faf3
Nathan Embery <nembery@juniper.net>
2018-04-16 16:59:10 -0400

    add disclaimer

--- wistar.py
+++ wistar.py
@@ -1,3 +1,21 @@
+#
+# DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER
+#
+# Copyright (c) 2015 Juniper Networks, Inc.
+# All rights reserved.
+#
+# Use is subject to license terms.
+#
+# Licensed under the Apache License, Version 2.0 (the ?License?); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Simple Salt dynamic roster module to pull all hosts from a wistar topology
 # to use first set some ENV variables like
 # bash: export WISTAR_URL='http://10.10.10.10:8080/topologies/27/'
@@ -78,5 +96,4 @@ def ret_glob_minions(hosts, tgt):
             matched_minions[minion] = hosts[minion]
 
     return matched_minions
-
 
